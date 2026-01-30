472686ebfe8ca91dae09670f8f245fb78c01ae49
Bei_Nicolas <bei.nicolas8484@gmail.com>
2026-01-30 15:19:41 +0100

    Surfer_Jump

--- Assets/Features/SurferController.cs
+++ Assets/Features/SurferController.cs
@@ -5,8 +5,16 @@ public class SurferController : MonoBehaviour
 {
     [SerializeField] private float minX = -6f;
     [SerializeField] private float maxX = 6f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private float jumpHoldForce = 20f;
+    [SerializeField] private float maxJumpHoldTime = 0.2f;
 
-
+    private float jumpHoldTimer;
+    private bool isJumping;
+    private bool isGrounded;
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private bool jumpPressed;
@@ -18,7 +26,10 @@ public class SurferController : MonoBehaviour
 
     void FixedUpdate()
     {
+        CheckGround();
         HandleMovement();
+        HandleJump();
+
     }
 
     void HandleMovement()
@@ -34,9 +45,32 @@ public class SurferController : MonoBehaviour
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
         transform.position = pos;
     }
+    void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
+        Debug.Log(isGrounded);
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
 
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+    void HandleJump()
+    {
+        if (jumpPressed && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
-
+            jumpPressed = false;
+        }
+    }
 
     // === INPUT SYSTEM EVENTS ===
 
